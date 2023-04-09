6792b9d48ca30b4dc943960c0603d95096f839bf
Sajjad <Serajzadeh.work@gmail.com>
2023-04-09 11:14:27 +0330

    [REFACTOR] Exception refactoring

--- src/main/java/ir/pod/podiotmockdevice/PodiotMockDeviceApplication.java
+++ src/main/java/ir/pod/podiotmockdevice/PodiotMockDeviceApplication.java
@@ -33,7 +33,38 @@ public class PodiotMockDeviceApplication {
         SpringApplication.run(PodiotMockDeviceApplication.class, args);
         List<Device> listDevices = new ArrayList<>();
 
-        listDevices.add(new PowerSwitch("22xza8wflbn", "IMMNA153Z7K1BI3DX2I4S4S")); //13- PowerSwitch Branch ID: 1605 طوس کهن -- invalid
+        listDevices.add(new IrTransmitter("xd51v1hydmj", "96T4JAIGD4VANIGWG0TMFM6"));
+        listDevices.add(new IrTransmitter("ktunhbi5j7c", "CBSTBJQH3IPDZR2BY3HNMIK"));
+
+        listDevices.add(new SmartRelay("ft4wq19lkdw", "LL28CHAXWXGE2KLEQ89OSHD"));
+        listDevices.add(new SmartRelay("9ukgpyjsm6p", "V7T99RHL214OQLLILS6ECHY"));
+        listDevices.add(new SmartRelay("ro5qfc4leeb", "677KXSMW74D261W680396O8"));
+
+        listDevices.add(new DoorSensor("dxxpivhirws", "XA0DG4PC1L3ZQBNURV92QKF"));
+        listDevices.add(new DoorSensor("6cgu891dk7o", "WJDK3JD2XB2LG003IZXYCFD"));
+        listDevices.add(new DoorSensor("2vriuq7njz7", "NMBJ8JGPPKOOI0O5INREI5B"));
+
+        listDevices.add(new Thermostat("cfsdkffc050", "895G9VSV5DTODZ5B4SZXI9U"));
+        listDevices.add(new Thermostat("azmtolm7i2u", "2TBOK3W0ITJE3LX437XULH3"));
+
+        listDevices.add(new PowerSwitch("22xza8wflbn", "IMMNA153Z7K1BI3DX2I4S4S"));
+
+        listDevices.add(new SecuritySystem("b54xbwda7fv", "ZWQIC89MAHMEHL60SA6UGOM"));
+        listDevices.add(new SecuritySystem("22az6p6oui3", "MR0CMQ4LKXIBMQ8HSE4DXXF"));
+
+        listDevices.add(new AirQuality("oeob5pteetk", "DZG55QAH5HQSSPFETV0OA7H"));
+
+        listDevices.add(new SecurityCamera("6wh2r8oxazl", "S97AQYQR6IQO1XVJ8JW2JDG"));
+        listDevices.add(new SecurityCamera("u2k9fbfvmnw", "F4SYP755REMIIOAVV1UM19Q"));
+        listDevices.add(new SecurityCamera("peg7k230v3n", "54413VUXYW7JQ601B1C6SC3"));
+
+        listDevices.add(new PowerMeter("ikdxl6z0r0e", "A14EZ2GV7A2YAUPFBZP55KB"));
+
+        listDevices.add(new MovementDetection("7rinzm131en", "BG9BKSNLCDRRHCQY99KNVGD"));
+
+        listDevices.add(new WaterMeter("9q4jvnhj3s4", "C5GCH0D3CJYP4D0PJB6PD1Z"));
+
+
 
 
 
@@ -45,16 +76,16 @@ public class PodiotMockDeviceApplication {
             mqttConnectionOptions.setCleanSession(true);
             mqttConnectionOptions.setAutomaticReconnect(true);
 //            mqttConnectionOptions.setConnectionTimeout(10);
-
+            device.getClass().getName();
             try {
                 mqttClient.connect(mqttConnectionOptions);
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("##################      Exception Happened Connection Failed      ##################");
+                System.out.println("##################      Exception Happened Connection Failed  ⛔   ##################");
             }
             do {
                 Thread.sleep(2000);
-                System.out.println("##################                  ReCONNECT                     ##################");
+                System.out.println("##################                                ReCONNECT                                 🟡 ##################");
 
             } while (!mqttClient.isConnected());
 
@@ -85,19 +116,25 @@ public class PodiotMockDeviceApplication {
                             if (device instanceof Actuator) {
                                 ((Actuator) device).processInput(mqttClient, reportedTopic, dto);
                             }
+                        } catch (JsonProcessException e) {
+                            System.out.println("##################      Json Parse Exception Happened Connection Failed  ⛔   ##################");
+                            throw new RuntimeException(e);
+                        } catch (MqttException e) {
+                            System.out.println("##################          MQTT Exception Happened Connection Failed    ⛔   ##################");
+                            throw new RuntimeException(e);
                         } catch (Exception e) {
                             log.error("sdkljfkjldsf");
                             e.printStackTrace();
                         }
                     });
-            System.out.println("##################        Device ({})  Connected         ##################"
+            System.out.println("##################                      Device ({})  Connected                     🟢 ##################"
                     .replace("{}", device.getDeviceId()));
 
 
             if (device instanceof Sensor) {
                 new Thread(() -> {
                     while (!"god is dead".isEmpty()) {
-                        System.out.println("##################    Sensors information published      ##################");
+                        System.out.println("##################     Sensors information published Type: " + device.getClass().getSimpleName() + " DeviceId: " + device.getDeviceId()  + "    ##############");
                         try {
                             ((Sensor) device).report(mqttClient, reportedTopic);
                             Thread.sleep(10000);
@@ -112,7 +149,7 @@ public class PodiotMockDeviceApplication {
                 }).start();
             }
         }
-        System.out.println("##################            Waiting for get message 🤬          ##################");
+        System.out.println("##################                         Waiting for get message                       🟡 ##################");
 
     }
 
